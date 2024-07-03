84cddf21be821f40ccc0e62f98dcd9e4f46f25f7
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-07-03 23:43:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ let snake = [];
 let direction = 'right';
 let food = [];
 let score = 0;
+
+const gameBoard = document.getElementById("game-board");
+
+function initGame(){
+    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, ${CELL_SIZE}px)`;
+    gameBoard.style.gridTemplateAreas = `repeat(${BOARD_SIZE}, ${CELL_SIZE}px)`;
+
+    for (let i = 0; i < INITIAL_SNAKE_LENGTH; i++) {
+        snake.push({x: INITIAL_SNAKE_LENGTH - 1 - i, y: 0});
+    }
+
+    placeFood();
+    gameLoop();
+}
+
+function gameLoop() {
+    moveSnake();
+    drawGame();
+    setTimeout(gameLoop, 100);
+}
+
+initGame(); 

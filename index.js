9623b0ee4fb232fa63492ca548b34b2f169d9272
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-07-03 23:59:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ function draw() {
 function drawSnake() {
     snake.forEach((segment) => {
         const snakeElement = createGameElement('div',
-             'snake');
+        'snake');
+        setPosition(snakeElement, segment);     
     });
+}
+
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+function setPosition(element, position){
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;    
 }
